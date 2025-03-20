e04fdb2586efed77e8a0cd42271ab874a101a44f
Nejc Vivod <nejc.vivod@mobiliti.si>
2025-03-20 15:09:21 +0100

    Bump to version 25.03.20 (matrix-rust-sdk/feature/federated-flag 82005f76254aeb75f5ea136175c9e71b6bc1ff24)

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
-let checksum = "80328d3190d19e003931c04eacade4056cbc90e89049190986e0dbe3d6de8aeb"
+let checksum = "78106f33e19303b8ba2aa0cd2c4a7cc8290131f976db95307adf2aeb846e2b29"
 let version = "25.03.20"
 let url = "https://github.com/nejcvivod/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 let package = Package(
